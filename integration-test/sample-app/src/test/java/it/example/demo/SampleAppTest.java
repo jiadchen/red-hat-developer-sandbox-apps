f34b2199612400ea0c34796a82b416389daae137
jiadchen <jiadchen@redhat.com>
2024-07-30 18:32:34 +0900

    version up for testing pipeline

--- integration-test/sample-app/src/test/java/it/example/demo/SampleAppTest.java
+++ integration-test/sample-app/src/test/java/it/example/demo/SampleAppTest.java
@@ -21,7 +21,7 @@ public class SampleAppTest {
   @Test
   public void sayHelloTest() {
     // Arrange
-    String expected = "Hello, openshift! version:1.0.43";
+    String expected = "Hello, openshift! version:1.0.44";
     int expectedStatusCode = 200;
 
     // Act
