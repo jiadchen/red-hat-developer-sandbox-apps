1053bbc3acb887e06d0f36d320d3414428df74cb
jiadchen <jiadchen@redhat.com>
2024-07-30 21:18:02 +0900

    version up for testing pipeline

--- apps/sample-app/src/main/java/com/example/demo/controller/HelloController.java
+++ apps/sample-app/src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
   @GetMapping("/hello")
   public String sayHello() {
-    return "Hello, openshift! version:1.0.44";
+    return "Hello, openshift! version:1.0.45";
   }
 }
